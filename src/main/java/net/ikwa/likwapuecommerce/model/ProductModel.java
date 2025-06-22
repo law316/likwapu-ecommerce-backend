41f6c6be03e0ad42f2a7795da2562f6f30aca3d8
Joshua <sector31619@gmail.com>
2025-06-22 21:40:26 +0100

    registration logic running hosted to test frontend logic, jar file made finally

--- src/main/java/net/ikwa/likwapuecommerce/model/ProductModel.java
+++ src/main/java/net/ikwa/likwapuecommerce/model/ProductModel.java
@@ -1,4 +1,44 @@
 package net.ikwa.likwapuecommerce.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.springframework.boot.autoconfigure.web.WebProperties;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
 public class ProductModel {
+   @Id
+   @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String productName ;
+    private String productDescription ;
+    private String productPrice ;
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getProductDescription() {
+        return productDescription;
+    }
+
+    public void setProductDescription(String productDescription) {
+        this.productDescription = productDescription;
+    }
+
+    public String getProductPrice() {
+        return productPrice;
+    }
+
+    public void setProductPrice(String productPrice) {
+        this.productPrice = productPrice;
+    }
 }
